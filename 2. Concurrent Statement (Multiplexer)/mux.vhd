44becf46696d1af063f09dfe3c84f854953ca52c
Arijeet Laga <43781731+arijeet11@users.noreply.github.com>
2020-09-29 01:47:38 +0200

    Update mux.vhd
    Added comments

--- 2. Concurrent Statement (Multiplexer)/mux.vhd
+++ 2. Concurrent Statement (Multiplexer)/mux.vhd
@@ -1,20 +1,20 @@
-library IEEE;
+library IEEE; 						-- Standard Library
 use IEEE.STD_LOGIC_1164.ALL;
 
 
-entity mux is
-    Port ( a : in  STD_LOGIC;
+entity mux is 						-- I/O declaration
+    Port ( a : in  STD_LOGIC; 				-- Note: Signals having same nature can be declared in a single line
            b : in  STD_LOGIC;
            c : in  STD_LOGIC;
            d : in  STD_LOGIC;
-           s : in  STD_LOGIC_VECTOR(1 downto 0);
+           s : in  STD_LOGIC_VECTOR(1 downto 0); 	-- This represents a 2 bit bus 
            x : out  STD_LOGIC);
 end mux;
 
 architecture rtl of mux is
 begin
 
-with s select
+with s select 						-- Implementation of "with-select" conditional logic
 x <= 	a when "00",
 		b when "01",
 		c when "10",
