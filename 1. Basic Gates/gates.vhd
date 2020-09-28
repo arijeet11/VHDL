a17258d22223c44912bdd5f5d118f7970e71da4c
Arijeet Laga <43781731+arijeet11@users.noreply.github.com>
2020-09-28 21:42:30 +0200

    Update gates.vhd
    Comments modified

--- 1. Basic Gates/gates.vhd
+++ 1. Basic Gates/gates.vhd
@@ -3,20 +3,20 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity gates is
+entity gates is 				-- I/O declaration
 	port
 	(
-		a,b : in std_logic;
-		c,f,h,k : out std_logic
+		a,b : in std_logic; 		-- Input signals
+		c,f,h,k : out std_logic 	-- Output signals
 	);
 end entity;
 
-architecture rt1 of gates is	
+architecture rt1 of gates is			-- Describing the behaviour and structure of the design
 begin
 
-	c <= a and b;	-- AND operation
-	f <= a or b; 	-- OR operation
-	h <= not a;		-- NOT operation
+	c <= a and b;				-- AND operation
+	f <= a or b; 				-- OR operation
+	h <= not a;				-- NOT operation
 	k <= (a or b) and (not(a and b));	-- XOR operation
 
 end rt1;
